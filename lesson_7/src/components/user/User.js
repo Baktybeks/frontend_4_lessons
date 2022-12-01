249d12bb4c87249ed6501df1b6f8fa5282507049
Baktybeks <baktybeks@gmail.com>
2022-12-02 02:16:40 +0600

    lesson 7

--- lesson_7/src/components/user/User.js
+++ lesson_7/src/components/user/User.js
@@ -5,16 +5,16 @@ function User({info}) {
 
     return (
         <Col xl={3}>
-        <Card style={{ width: '100%', marginBottom:"25px" }}>
-            <Card.Body>
-                <Card.Title>{info.name}</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
+            <Card style={{width: '100%', marginBottom: "25px"}}>
+                <Card.Body>
+                    <Card.Title>{info.name}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </Col>
     );
 }
